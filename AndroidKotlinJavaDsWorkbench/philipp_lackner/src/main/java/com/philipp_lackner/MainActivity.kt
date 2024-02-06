f720b884535a58bd6ad71875f4a982ff86bf034c
VinitKr <vinitkr28@gmail.com>
2024-02-06 23:12:53 +0530

    swap to dismiss with material3

--- AndroidKotlinJavaDsWorkbench/philipp_lackner/src/main/java/com/philipp_lackner/MainActivity.kt
+++ AndroidKotlinJavaDsWorkbench/philipp_lackner/src/main/java/com/philipp_lackner/MainActivity.kt
@@ -3,44 +3,58 @@ package com.philipp_lackner
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkVertically
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.DismissDirection
+import androidx.compose.material3.DismissState
+import androidx.compose.material3.DismissValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.SwipeToDismiss
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDismissState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.philipp_lackner.ui.theme.AndroidKotlinJavaDsWorkbenchTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.philipp_lackner.ui.theme.PhilippLacknerModuleTheme
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AndroidKotlinJavaDsWorkbenchTheme {
+            PhilippLacknerModuleTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidKotlinJavaDsWorkbenchTheme {
-        Greeting("Android")
     }
 }
---
+++ AndroidKotlinJavaDsWorkbench/philipp_lackner/src/main/java/com/philipp_lackner/swap_to_dismiss/readMe.md
@@ -0,0 +1,3 @@
+https://youtu.be/IlI6GgC_j78?si=FyDoBatfU8Yl-4RG
+
+https://github.com/philipplackner/SwipeToDeleteCompose
