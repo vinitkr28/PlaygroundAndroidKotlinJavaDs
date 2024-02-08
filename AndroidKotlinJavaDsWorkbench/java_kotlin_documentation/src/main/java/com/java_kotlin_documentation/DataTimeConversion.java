89d3dd156f404e68df42e73a413131a967c6751c
VinitKr <vinitkr28@gmail.com>
2024-02-09 01:28:36 +0530

    new module java_kotlin_documentation

---
+++ AndroidKotlinJavaDsWorkbench/java_kotlin_documentation/src/main/java/com/java_kotlin_documentation/DataTimeConversion.java
@@ -0,0 +1,17 @@
+package com.java_kotlin_documentation;
+
+/**
+ * This class deals with date time conversion.
+ */
+public class DataTimeConversion {
+
+    /**
+     *
+     * @param message print statement
+     * @return blank
+     */
+    public String printDateTime(String message){
+
+        return "";
+    }
+}
