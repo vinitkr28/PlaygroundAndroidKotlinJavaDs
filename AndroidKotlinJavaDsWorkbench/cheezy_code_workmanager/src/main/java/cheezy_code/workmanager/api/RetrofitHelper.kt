1b1bdb1959dfd6063ac060a7968ba060571d028d
VinitKr <vinitkr28@gmail.com>
2024-02-21 23:28:25 +0530

    quote api with worker

---
+++ AndroidKotlinJavaDsWorkbench/cheezy_code_workmanager/src/main/java/cheezy_code/workmanager/api/RetrofitHelper.kt
@@ -0,0 +1,15 @@
+package cheezy_code.workmanager.api
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitHelper {
+    private const val BASE_URL = "https://quotable.io/"
+
+    fun getInstance() : Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+}
