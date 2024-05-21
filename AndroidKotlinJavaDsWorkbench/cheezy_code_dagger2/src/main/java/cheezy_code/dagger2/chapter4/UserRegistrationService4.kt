db9c2150b70c057466d21185c25e3a909c657149
VinitKr <vinitkr28@gmail.com>
2024-05-22 03:39:34 +0530

    Dagger 2 Field Injection - @Inject Tutorial - CheezyCode (Hindi) #4 https://youtu.be/EDUK-IptF9w?si=ZCGPXdV96mg8PBbL Cheezy Code cheezy_code/dagger2/4-Field-Injection-Inject

--- AndroidKotlinJavaDsWorkbench/cheezy_code_dagger2/src/main/java/cheezy_code/dagger2/chapter4/UserRegistrationService4.kt
+++ AndroidKotlinJavaDsWorkbench/cheezy_code_dagger2/src/main/java/cheezy_code/dagger2/chapter4/UserRegistrationService4.kt
@@ -4,17 +4,10 @@ import javax.inject.Inject
 
 class UserRegistrationService4 @Inject constructor(
     private val userRepository: UserRepository4,
-    private val emailService: EmailService4
+    private val notificationService: NotificationService4
 ) {
     fun registerUser(email: String, password: String) {
         userRepository.saveUser(email, password)
-        emailService.send(email, "abc@xyz.com", "user registered")
+        notificationService.send(email, "abc@xyz.com", "user registered")
     }
 }
-
-/*
-* Unit Testing
-* Single Responsibility
-* Lifetime of these objects
-* Extensible
-* */
