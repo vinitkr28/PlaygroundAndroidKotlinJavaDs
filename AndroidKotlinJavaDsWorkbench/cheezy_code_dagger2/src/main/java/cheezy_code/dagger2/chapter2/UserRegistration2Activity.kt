5284767defda0f7d5d342bb23b80b2cbffdac34e
VinitKr <vinitkr28@gmail.com>
2024-05-22 02:23:33 +0530

    Dagger 2 & Manual Dependency Injection Tutorial - CheezyCode (Hindi) #2 https://youtu.be/cg0yCHW2Keg?si=eCvlMWTsICW7v1AF Cheezy Code cheezy_code/dagger2/2-Manual-Dependency-Injection
    * code cleanup

---
+++ AndroidKotlinJavaDsWorkbench/cheezy_code_dagger2/src/main/java/cheezy_code/dagger2/chapter2/UserRegistration2Activity.kt
@@ -0,0 +1,28 @@
+package cheezy_code.dagger2.chapter2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import cheezy_code.dagger2.R
+
+class UserRegistration2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        /*
+        val userRegistrationService2a = UserRegistrationService2a()
+        userRegistrationService2a.registerUser("abc@xyz.com", "1234567890")
+        */
+
+
+        val emailService2 = EmailService2()
+        val userRepository2 = UserRepository2()
+
+        val userRegistrationService2b = UserRegistrationService2b(
+            emailService = emailService2,
+            userRepository = userRepository2
+        )
+        userRegistrationService2b.registerUser("abc@xyz.com", "1234567890")
+
+    }
+}
