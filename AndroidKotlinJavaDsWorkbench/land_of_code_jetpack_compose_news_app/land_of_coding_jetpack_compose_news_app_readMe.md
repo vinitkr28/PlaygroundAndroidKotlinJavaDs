7f095b4b0c400ce87a85aaa22a034ec5869fbb38
VinitKr <vinitkr28@gmail.com>
2024-07-20 03:23:55 +0530

    Splash Screen - Jetpack Compose Clean Architecture News App - part 2 https://youtu.be/5cEyV9GwlnA?si=qBJ9gtXAgrRt7JSs)
    Land of coding

--- AndroidKotlinJavaDsWorkbench/land_of_code_jetpack_compose_news_app/land_of_coding_jetpack_compose_news_app_readMe.md
+++ AndroidKotlinJavaDsWorkbench/land_of_code_jetpack_compose_news_app/land_of_coding_jetpack_compose_news_app_readMe.md
@@ -5,3 +5,7 @@
 https://github.com/lofcoding/NewsApp/tree/master
 
 ### 1/19. [Introduction - Jetpack Compose Clean Architecture News App - part 1](https://youtu.be/9AekfR-EI-U?si=O-SQaCN_J9zDkeS3)
+
+
+
+### 2/19. [Splash Screen - Jetpack Compose Clean Architecture News App - part 2](https://youtu.be/5cEyV9GwlnA?si=qBJ9gtXAgrRt7JSs)
--- AndroidKotlinJavaDsWorkbench/land_of_code_jetpack_compose_news_app/src/main/java/land_of_code/jetpack_compose/news_app/MainActivity.kt
+++ AndroidKotlinJavaDsWorkbench/land_of_code_jetpack_compose_news_app/src/main/java/land_of_code/jetpack_compose/news_app/MainActivity.kt
@@ -11,11 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import land_of_code.jetpack_compose.news_app.ui.theme.AndroidKotlinJavaDsWorkbenchTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         enableEdgeToEdge()
         setContent {
             AndroidKotlinJavaDsWorkbenchTheme {
