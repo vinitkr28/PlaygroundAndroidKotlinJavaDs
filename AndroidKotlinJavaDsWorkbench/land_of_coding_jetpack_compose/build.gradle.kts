1958310e975b348f0166303ab537010c5fa0ffed
VinitKr <vinitkr28@gmail.com>
2024-09-16 20:39:40 +0530

    Text in Jetpack Compose From Beginner to Pro https://youtu.be/mfNCRRY-uho?si=bUCRSRmJTMnR_6t9
    AppBarExampleActivity.kt
    SecondActivity.kt
    
    Land of coding

--- AndroidKotlinJavaDsWorkbench/land_of_coding_jetpack_compose/build.gradle.kts
+++ AndroidKotlinJavaDsWorkbench/land_of_coding_jetpack_compose/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.activity:activity:1.9.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
@@ -68,4 +69,6 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    implementation ("androidx.compose.material:material-icons-extended-android:1.5.4")
 }
