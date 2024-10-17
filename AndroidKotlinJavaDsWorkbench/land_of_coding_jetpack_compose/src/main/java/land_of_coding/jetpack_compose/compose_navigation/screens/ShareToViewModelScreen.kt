ec0efcba4ad7e16e541ff29129291fbe768c8040
VinitKr <vinitkr28@gmail.com>
2024-10-18 03:54:43 +0530

    19/19. [Jetpack compose navigation - arguments] (https://youtu.be/bj6OjSjPM1I?si=9VjyJOdRKdTXDc1A)
    * shared view model
    
    Land of coding

--- AndroidKotlinJavaDsWorkbench/land_of_coding_jetpack_compose/src/main/java/land_of_coding/jetpack_compose/compose_navigation/screens/ShareToViewModelScreen.kt
+++ AndroidKotlinJavaDsWorkbench/land_of_coding_jetpack_compose/src/main/java/land_of_coding/jetpack_compose/compose_navigation/screens/ShareToViewModelScreen.kt
@@ -10,14 +10,8 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
